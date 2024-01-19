b376b0f1bd64684b040e5ff1b52fe5d68322c630
MalavikaAjith <126668248+MalavikaAjith@users.noreply.github.com>
2024-01-19 11:39:58 +0530

    Add files via upload

---
+++ pro1.django/proj1/app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from . models import card
+
+
+# Create your views here.
+def func(request):
+    imag = {
+        'img': card.objects.all()
+    }
+    return render(request,'pro1.html',imag)
